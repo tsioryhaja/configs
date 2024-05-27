194ab0a018fd2de6647406cb104d086700f56ac7
Tsiory RAZAFINDRAINIBE <thvrazafindrainibe@absys.fr>
2024-05-27 15:59:15 +0200

    using a no theme neovim

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,38 +1,38 @@
-local status, kanagawa = pcall(require, "kanagawa")
-if (not status) then
-  return
-end
-
-kanagawa.setup{
-  -- theme = "wave",
-  -- theme = "dragon",
-  -- theme = "lotus",
-  colors = {
-    theme = {
-      all = {
-        ui = {
-          bg_gutter = "none"
-        }
-      }
-    }
-  }
-}
-
-vim.cmd":colorscheme kanagawa-wave"
+-- local status, kanagawa = pcall(require, "kanagawa")
+-- if (not status) then
+--   return
+-- end
+--
+-- kanagawa.setup{
+--   -- theme = "wave",
+--   -- theme = "dragon",
+--   -- theme = "lotus",
+--   colors = {
+--     theme = {
+--       all = {
+--         ui = {
+--           bg_gutter = "none"
+--         }
+--       }
+--     }
+--   }
+-- }
+--
+-- vim.cmd":colorscheme kanagawa-wave"
 -- vim.cmd":colorscheme kanagawa-dragon"
 -- vim.cmd":colorscheme kanagawa-lotus"
 
 -- local ubuntuSelect = '#77216F'
 -- local ubuntuSelect = '#5E2750'
--- local greySelect = 'grey40'
+local greySelect = 'grey40'
 -- local darkGrey = '#333333'
---
---
--- vim.cmd':hi Pmenu guibg=none'
--- vim.cmd(':hi CursorLine guibg='..ubuntuSelect)
--- vim.cmd(':hi TelescopePreviewLine guibg='..ubuntuSelect)
--- vim.cmd(':hi TelescopeSelection guibg='..ubuntuSelect)
--- vim.cmd(':hi Visual guibg='..ubuntuSelect)
+
+
+vim.cmd':hi Pmenu guibg=none'
+vim.cmd(':hi CursorLine guibg='..greySelect)
+vim.cmd(':hi TelescopePreviewLine guibg='..greySelect)
+vim.cmd(':hi TelescopeSelection guibg='..greySelect)
+vim.cmd(':hi Visual guibg='..greySelect)
 
 -- ubuntu colorscheme origin
 -- https://design.ubuntu.com/brand/colour-palette
