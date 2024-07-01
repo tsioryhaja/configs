89251892c04ae12c799d8bad9011c343d467b32e
Tsiory RAZAFINDRAINIBE <thvrazafindrainibe@absys.fr>
2024-07-01 13:31:40 +0200

    with this i can debug fastapi with neovim

--- nvim/lua/dap_adapters/python.lua
+++ nvim/lua/dap_adapters/python.lua
@@ -16,11 +16,19 @@ local function getParams(config)
       end
     end
   elseif config.type == "server" then
-    if config.localDebugpyLauncherCommand then
-      result = config.localDebugpyLauncherCommand
-    end
+    -- if config.localDebugpyLauncherCommand then
+    --   result = config.localDebugpyLauncherCommand
+    -- end
   end
   return result
+end
+
+local function table_contains(_table, _value)
+  local found = false
+  for _, v in pairs(_table) do
+    found = found or v == _value
+  end
+  return found
 end
 
 function DebugpySocketsHandler(session, body)
@@ -30,12 +38,15 @@ function DebugpySocketsHandler(session, body)
   if #sockets > 0 then
     local port = 0
     for _, value in pairs(sockets) do
-      print(vim.json.encode(value))
-      if value.port > port then
+      if value.port > port and value.internal == false then
+        -- print(vim.json.encode(value))
         port = value.port
       end
     end
-    if port > 0 then
+    if not session.debugpy_connected_ports then
+      session.debugpy_connected_ports = {}
+    end
+    if port > 0 and not table_contains(session.debugpy_connected_ports, port) then
       local opts = {
         "C:\\Users\\tsiory_re\\.vscode\\extensions\\ms-python.debugpy-2024.6.0-win32-x64\\bundled\\libs\\debugpy\\launcher",
         -- "-m",
@@ -43,11 +54,13 @@ function DebugpySocketsHandler(session, body)
         tostring(port),
         "--",
       }
-      local newargs = getParams(session.config)
-      for _, v in pairs(newargs) do
-        table.insert(opts, v)
-      end
-      print(port)
+      -- local newargs = getParams(session.config)
+      -- for _, v in pairs(newargs) do
+      --   table.insert(opts, v)
+      -- end
+      -- print(port)
+      -- print(vim.json.encode(opts))
+      table.insert(session.debugpy_connected_ports, port)
       uv.spawn('python', {args = opts})
     end
   end
